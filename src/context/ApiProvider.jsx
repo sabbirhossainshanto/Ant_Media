4f4d6fb0034254d99fb78e5d83cbeec6f943bd09
sabbirHossain <sabbirshnt@gmail.com>
2024-11-26 18:11:51 +0600

    done

--- src/context/ApiProvider.jsx
+++ src/context/ApiProvider.jsx
@@ -1,49 +1,26 @@
 import { createContext, useEffect, useState } from "react";
 import { getSetApis } from "../api/config";
-import { API, settings } from "../api";
 import notice from "../../notice.json";
 export const ApiContext = createContext(null);
 const ApiProvider = ({ children }) => {
   const [noticeLoaded, setNoticeLoaded] = useState(false);
-  const [logo, setLogo] = useState("");
   const baseUrl = notice?.result?.settings?.baseUrl;
+
   useEffect(() => {
-    getSetApis(setNoticeLoaded, baseUrl);
+    const callApi = () => {
+      getSetApis(setNoticeLoaded, baseUrl);
+    };
+    callApi();
+
+    const intervalId = setInterval(callApi, 1000);
+    return () => clearInterval(intervalId);
   }, [noticeLoaded, baseUrl]);
-
-  useEffect(() => {
-    if (noticeLoaded) {
-      const logo = `${API.assets}/${settings.siteUrl}/logo.${settings.logoFormat}`;
-      setLogo(logo);
-      /* Theme css */
-      const link = document.createElement("link");
-      link.rel = "stylesheet";
-      link.type = "text/css";
-
-      if (settings.build === "production") {
-        link.href = `${API.assets}/${settings.siteUrl}/theme.css`;
-        document.head.appendChild(link);
-      } else {
-        link.href = `/src/assets/css/theme.css`;
-        document.head.appendChild(link);
-      }
-
-      /* Dynamically append  favicon  */
-      const FavIconLink = document.createElement("link");
-      FavIconLink.rel = "icon";
-      FavIconLink.type = "image/png";
-      FavIconLink.href = `${API.assets}/${settings.siteUrl}/favicon.png`;
-      document.head.appendChild(FavIconLink);
-      /* Site title */
-      document.title = settings.siteTitle;
-    }
-  }, [noticeLoaded]);
 
   if (!noticeLoaded) {
     return;
   }
 
-  const stateInfo = { logo };
+  const stateInfo = {};
   return (
     <ApiContext.Provider value={stateInfo}>{children}</ApiContext.Provider>
   );
