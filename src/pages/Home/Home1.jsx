8ed6388932322b5aa9b74109f49db021efda6652
sabbirHossain <sabbirshnt@gmail.com>
2024-11-27 12:20:06 +0600

    done

--- src/pages/Home/Home1.jsx
+++ src/pages/Home/Home1.jsx
@@ -24,7 +24,7 @@ const Home1 = () => {
       setSmallVideoPosition(data.result.settings.small_video?.position);
 
       if (!isInitialLoad.current) {
-        setAnimationStage("sliding-in");
+        setAnimationStage("sliding-out");
       }
       isInitialLoad.current = false;
     }
